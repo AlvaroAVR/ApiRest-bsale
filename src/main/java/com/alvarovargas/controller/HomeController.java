41b809f4265bb96963d6573ad66da2c4a76da4bf
AlvaroAVR <101934732+AlvaroAVR@users.noreply.github.com>
2022-11-13 19:35:38 -0300

    agregado controlador

---
+++ src/main/java/com/alvarovargas/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.alvarovargas.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alvarovargas.model.Product;
+import com.alvarovargas.service.ProductService;
+
+@RestController//etiqueta para que spring configure esta clase como controlador rest
+@RequestMapping("/api/v1/products")//ruta o mapping por defecto de la clase
+public class HomeController {
+
+	@Autowired//etiqueta para la inyeccion de dependencias
+	private ProductService productService;
+	
+	@GetMapping("/todos")//ruta o mapping al que respondera el metodo para mostrar todos los productos
+    public ResponseEntity<List<Product>> todos(){
+    	return ResponseEntity.ok(productService.listarProductos());
+    }
+    
+    @GetMapping("/buscar")//ruta o mapping al que respondera el metodo para mostrar el producto buscado
+    public ResponseEntity<List<Product>> buscarProductos(@RequestParam("query") String query){
+    	return ResponseEntity.ok(productService.buscarProductos(query));
+    }
+}
